9c915533fa484bdcf75e2d5c6d8381fe4873f18c
juiceghost <jg@renegade.se>
2021-04-20 12:05:37 +0200

    Bezkoder Create the Controller done

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
-//const bodyParser = require('body-parser');
+const cors = require("cors");
+
+var corsOptions = {
+    origin: "http://localhost:3000"
+  };
 
 const UserModel = require('./model/model');
 
@@ -14,16 +18,20 @@ require('./auth/auth');
 
 const routes = require('./routes/routes');
 const secureRoute = require('./routes/secure-routes');
+const secureAPIRoute = require('./routes/secure-api-routes');
 
 const app = express();
 
+app.use(cors(corsOptions));
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use('/', routes);
 
 // Plug in the JWT strategy as a middleware so only verified users can access this route.
 app.use('/user', passport.authenticate('jwt', { session: false }), secureRoute);
-
+app.use('/api', passport.authenticate('jwt', { session: false }), secureAPIRoute);
 // Handle errors.
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
